ec557c7680bd7058a6e0d808c2a4bd37bde408c6
msidemo-admin <kumbam96@students.rowan.edu>
2023-10-05 17:51:22 -0400

    Initial Commit

---
+++ src/main/java/org/bharghavi/demo/Main.java
@@ -0,0 +1,9 @@
+package org.bharghavi.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("Hello Bharghavi");
+    }
+}
